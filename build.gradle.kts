757c522e7abcf35e892835e91855011c41c8e830
BizCub <bizcubmine@gmail.com>
2026-08-24 12:58:26 +0200

    Update Simple Config Lib dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,8 @@ multiloader {
         dependency = "maven.modrinth:rrls:${getDep("rrls")}"
     )
     addDependency(
-        dependency = "io.github.bizcub:simple-config-lib:1.0-${mod.loader}+${mod.mc}"
+        dependency = "maven.modrinth:simple-config-lib:${getDep("simple-config-lib")}",
+        isPublishDepEnabled = true
     )
     val isClothConfigAvailable = !(isForge && scp > "1.21.3")
     addDependency(
